309e056adbcc4594f946ce0872e2aa7a83e29efc
zhengp0 <zhengp@uw.edu>
2020-04-18 19:19:50 -0700

    update: add the data module

--- src/odeopt/core/utils.py
+++ src/odeopt/core/utils.py
@@ -38,3 +38,43 @@ def linear_interpolate(t_target, t_org, x_org):
         return x_target.ravel()
     else:
         return x_target
+
+
+def _change_name(old_name, name_dict):
+    """Change the old name to new names.
+
+    Args:
+        old_name (str): String of old name.
+        name_dict (dict{str, str}): Rules map the old name to new name.
+
+    Returns:
+         str: New name.
+    """
+    assert isinstance(old_name, str)
+    assert isinstance(name_dict, dict)
+
+    if old_name in name_dict:
+        return name_dict[old_name]
+    else:
+        return old_name
+
+
+def change_names(old_names, name_dict):
+    """Change the old name to new names.
+
+    Args:
+        old_names (str | list{str}): String or a list of string of old names.
+        name_dict (dict{str, str}): Rules map the old name to new name.
+
+    Returns:
+         new_names (str | list{str}): New names.
+    """
+    if isinstance(old_names, list):
+        new_names = [
+            _change_name(name, name_dict)
+            for name in old_names
+        ]
+    else:
+        new_names = _change_name(old_names, name_dict)
+
+    return new_names
