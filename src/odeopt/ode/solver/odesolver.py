a22974179b322d2c704ef86e52b33a65d2744264
Aleksei Sholokhov <ak.sholokhov@gmail.com>
2020-04-20 16:36:24 -0700

    Add RK4 solver and a test for it (#2)

--- src/odeopt/ode/solver/odesolver.py
+++ src/odeopt/ode/solver/odesolver.py
@@ -12,6 +12,7 @@ from odeopt.core import utils
 class ODESolver:
     """ODE Solver.
     """
+
     def __init__(self, system, dt):
         """Constructor of the ODESovler.
 
@@ -48,6 +49,7 @@ class ODESolver:
 class ForwardEuler(ODESolver):
     """Forward Euler Solver.
     """
+
     def __init__(self, *args):
         super().__init__(*args)
 
@@ -61,8 +63,44 @@ class ForwardEuler(ODESolver):
         # linear interpolate the parameters
         params = utils.linear_interpolate(t_solve, t_params, params)
         for i in range(1, t_solve.size):
-            y_solve[:, i] = y_solve[:, i - 1] + self.dt*self.system(
+            y_solve[:, i] = y_solve[:, i - 1] + self.dt * self.system(
                 t_solve[i - 1], y_solve[:, i - 1], params[:, i - 1])
+
+        # linear interpolate the solutions.
+        y_solve = utils.linear_interpolate(t, t_solve, y_solve)
+        return y_solve
+
+
+class RK4(ODESolver):
+    """
+        4th order Runge-Kutta solver.
+    """
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def solve(self, t, init_cond, t_params, params):
+        """
+        Solver the ode
+        :param t:
+        :param init_cond:
+        :param t_params:
+        :param params:
+        :return:
+        """
+
+        t_solve = np.arange(np.min(t), np.max(t) + self.dt, self.dt / 2)
+        y_solve = np.zeros((init_cond.size, t_solve.size),
+                           dtype=init_cond.dtype)
+        y_solve[:, 0] = init_cond
+        # linear interpolate the parameters
+        params = utils.linear_interpolate(t_solve, t_params, params)
+        for i in range(2, t_solve.size, 2):
+            k1 = self.system(t_solve[i - 2], y_solve[:, i - 2], params[:, i - 2])
+            k2 = self.system(t_solve[i - 1], y_solve[:, i - 2] + self.dt / 2 * k1, params[:, i - 1])
+            k3 = self.system(t_solve[i - 1], y_solve[:, i - 2] + self.dt / 2 * k2, params[:, i - 1])
+            k4 = self.system(t_solve[i], y_solve[:, i - 2] + self.dt * k3, params[:, i])
+            y_solve[:, i] = y_solve[:, i - 2] + self.dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
         # linear interpolate the solutions.
         y_solve = utils.linear_interpolate(t, t_solve, y_solve)
--- tests/ode/test_ode_solver.py
+++ tests/ode/test_ode_solver.py
@@ -5,20 +5,20 @@
 """
 import numpy as np
 import pytest
-from odeopt.ode import ForwardEuler
+from odeopt.ode import ForwardEuler, RK4
 
 
 @pytest.fixture()
 def simple_system():
     def system(t, y, lam):
-        return lam[0]*y
+        return lam[0] * y
 
     return system
 
 
 @pytest.mark.parametrize('dt', [1e-1, 1e-2, 1e-3])
 @pytest.mark.parametrize('t_span', [np.array([0.0, 1.0])])
-@pytest.mark.parametrize(('params', 'init_cond','true_fun'),
+@pytest.mark.parametrize(('params', 'init_cond', 'true_fun'),
                          [(np.array([[-1.0]]),
                            np.array([1.0]),
                            lambda t: np.exp(-t))])
@@ -28,4 +28,19 @@ def test_forward_euler(simple_system, dt, t_span, init_cond, params, true_fun):
     result = true_fun(t)
     my_result = solver.solve(t, init_cond,
                              t, np.repeat(params, t.size, axis=1))[0]
-    assert np.sqrt(np.mean((result - my_result)**2)) < dt*5.0
+    assert np.sqrt(np.mean((result - my_result) ** 2)) < dt * 5.0
+
+
+@pytest.mark.parametrize('dt', [1e-1, 1e-2, 1e-3])
+@pytest.mark.parametrize('t_span', [np.array([0.0, 1.0])])
+@pytest.mark.parametrize(('params', 'init_cond', 'true_fun'),
+                         [(np.array([[-1.0]]),
+                           np.array([1.0]),
+                           lambda t: np.exp(-t))])
+def test_runge_kutta_4th(simple_system, dt, t_span, init_cond, params, true_fun):
+    solver = RK4(simple_system, dt)
+    t = np.arange(t_span[0], t_span[1] + dt, dt)
+    result = true_fun(t)
+    my_result = solver.solve(t, init_cond,
+                             t, np.repeat(params, t.size, axis=1))[0]
+    assert np.sqrt(np.mean((result - my_result) ** 2)) < dt ** 4 * 5.0
