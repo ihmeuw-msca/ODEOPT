49f41d1fa2ecb2e3d9a052b40230cd0b4c2fb2e5
Marlena Bannick <mnorwood@uw.edu>
2020-05-23 12:42:44 -0700

    Updated version

--- src/odeopt/__about__.py
+++ src/odeopt/__about__.py
@@ -7,7 +7,7 @@ __title__ = "odeopt"
 __summary__ = "Optimization-based ODE Model"
 __uri__ = "https://github.com/ihmeuw-msca/ODEOPT"
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __author__ = "The IHME Math Sciences Team"
 __email__ = "ihme.math.sciences@gmail.com"
